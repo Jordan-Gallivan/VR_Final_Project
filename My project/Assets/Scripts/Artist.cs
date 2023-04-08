41b7fb7da15ff9c400bb5a3447a34cebc1ae6160
Jordan-Gallivan <jmatt03@gmail.com>
2023-04-07 20:09:17 -0400

    Artist and Period objects

---
+++ My project/Assets/Scripts/Artist.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artist
+{
+    public string artistName { get; set; }
+
+    private List<string> brief;
+    public List<string> Brief => brief;
+
+    private string bio { get; set; }
+
+    public Artist()
+    {
+        artistName = "";
+        brief = new List<string>();
+        bio = "";
+    }
+
+    public void addToBrief(string addition)
+    {
+        brief.Add(addition);
+    }
+}
---
+++ My project/Assets/Scripts/Period.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Period
+{
+    private string periodName;
+    private string PeriodName
+    {
+        get => periodName;
+        set => periodName = value;
+    }
+    public List<Artist> artists { get; }
+
+    public Period()
+    {
+        periodName = "";
+        artists = new List<Artist>();
+    }
+
+    public Period(string name) : this()
+    {
+        periodName = name;
+    }
+
+    public void addArtist(Artist artist)
+    {
+        artists.Add(artist);
+    }
+}
